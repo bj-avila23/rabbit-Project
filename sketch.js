cd71a295a9615cd17ed0b8b6e72b72b302b9145b
bj-avila23 <88911160+bj-avila23@users.noreply.github.com>
2021-08-13 16:01:27 -0700

    Add files via upload
    descargamos archivos del rabbit

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple,leaves;
+var appleImg,leavesImg;
+var randomobjects;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg  = loadImage("apple.png");
+  leavesImg = loadImage("orangeLeaf.png");
+  
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage("garden",gardenImg);
+  
+
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage("rabbit",rabbitImg);
+
+}
+
+
+function draw() {
+  background(0);
+  
+  rabbit.x=World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+if(frameCount%80===0){
+  
+  randomobjects=Math.round(random(1,2));
+if(randomobjects===1){
+  createLeaves();
+}
+else if(randomobjects===2){
+  createApples();
+}
+}
+
+  drawSprites();
+}
+
+function createApples(){
+  apple=createSprite(random(50,350),40);
+  apple.velocityY=2;
+  apple.scale =0.05;
+  apple.addImage("apple",appleImg);
+  
+  
+  }
+
+function createLeaves(){
+  
+leaves=createSprite(random(50,350),40);
+leaves.velocityY=2;
+leaves.scale =0.05;
+leaves.addImage("leaves",leavesImg);
+
+}
+
